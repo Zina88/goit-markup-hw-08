3544499daf1efd0d10dc2a2468f8c145850d7f9d
Zina <zinasaenko@hotmail.com>
2022-04-28 18:03:01 +0300

    :herb:

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.classList.toggle("is-active");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
     body.classList.toggle("no-scroll");
